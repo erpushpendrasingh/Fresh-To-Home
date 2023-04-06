a7d680695e301d54eb88133aabdb18c2447ae71d
Pushpendra Singh <erpushpendrasingh8900@gmail.com>
2023-04-07 00:52:52 +0530

    package.json

--- src/Redux/AppReducer/action.js
+++ src/Redux/AppReducer/action.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import * as types from "./actionTypes";
 const getFish = (queryParams) => (dispatch) => {
      dispatch({ type: types.GET_FISH_REQUEST });
-     let url = "http://localhost:8080/fish";
+     let url = "https://moc-server-app.onrender.com/fish";
      return axios
           .get(url, queryParams)
 
@@ -20,7 +20,7 @@ const getFish = (queryParams) => (dispatch) => {
 const addProducts = (payload) => (dispatch) => {
      dispatch({ type: types.ADD_FISH_REQUEST });
      return axios
-          .post("http://localhost:8080/fish", payload)
+          .post("https://moc-server-app.onrender.com/fish", payload)
           .then((r) => {
                dispatch({
                     type: types.ADD_FISH_SUCCESS,
@@ -34,7 +34,7 @@ const addProducts = (payload) => (dispatch) => {
 const editProducts = (id, payload) => (dispatch) => {
      dispatch({ type: types.EDIT_PRODUCT_REQUEST });
      return axios
-          .patch(`http://localhost:8080/fish/${id}`, payload)
+          .patch(`https://moc-server-app.onrender.com/fish/${id}`, payload)
           .then((r) => {
                dispatch({ type: types.EDIT_PRODUCT_SUCCESS, payload: r.data });
           })
@@ -45,7 +45,7 @@ const editProducts = (id, payload) => (dispatch) => {
 const getByQ = (q) => (dispatch) => {
      dispatch({ type: types.GET_FISH_REQUEST });
      return axios
-          .get(`http://localhost:8080/fish?q=${q}`)
+          .get(`https://moc-server-app.onrender.com/fish?q=${q}`)
           .then((r) => {
                dispatch({
                     type: types.GET_FISH_SUCCESS,
@@ -60,7 +60,7 @@ const getByQ = (q) => (dispatch) => {
 const getCurrentProduct = (id) => (dispatch) => {
      dispatch({ type: types.GET_CURRENT_PRODUCT_REQUEST });
      return axios
-          .get(`http://localhost:8080/fish/${id}`)
+          .get(`https://moc-server-app.onrender.com/fish/${id}`)
           .then((res) =>
                dispatch({
                     type: types.GET_CURRENT_PRODUCT_SUCCESS,
